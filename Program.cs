a566bc6280223aaa137110c172468c3ed2c179c0
Sam Carbine <scarbine@me.com>
2021-08-31 10:17:07 -0500

    added values to the properties all echanscated objects

--- Program.cs
+++ Program.cs
@@ -7,16 +7,44 @@ namespace garys_garage
     {
         static void Main(string[] args)
         {
-            Zero fxs = new Zero();
-            Zero fx = new Zero();
-            Tesla mine = new Tesla();
-            Cessna c3 = new Cessna();
+            Zero fxs = new Zero()
+            {
+                MainColor = "Black",
+                MaxiumOccupancey = 4,
+                BatteryKwh = 175.25
+            };
+            Zero fx = new Zero()
+            {
+                MainColor = "Blue",
+                MaxiumOccupancey = 4,
+                BatteryKwh = 175.25
+            };
+            Tesla mine = new Tesla()
+            {
+                MainColor = "Red",
+                MaxiumOccupancey = 2,
+                BatteryKwh = 250.50
+            };
+            Cessna c3 = new Cessna()
+            {
+                MainColor = "White",
+                MaxiumOccupancey = 2,
+                FuelCapacity = 15.25
+            };
+
+            Ram Beast = new Ram(){
+                
+                MainColor = "Grey",
+                MaxiumOccupancey = 2,
+                FuelCapacity = 25.25
+            };
             List<Zero> electricVehicles = new List<Zero>() { fx , fxs};
 
             fxs.Drive();
             fx.Drive();
             mine.Drive();
             c3.Drive();
+            Beast.Drive();
 
 
         }
--- Ram.cs
+++ Ram.cs
@@ -11,5 +11,10 @@ namespace garys_garage
         {
 
         }
+
+        public override void Drive()
+        {
+            Console.WriteLine($"The {MainColor} Ram just ran over you {MaxiumOccupancey} people in it. Ouch!");
+        }
     }
 }
